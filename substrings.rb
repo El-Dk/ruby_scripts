3060ae7f50739824c0544506cb954cf6001e0f5c
Derek <briannish@gmail.com>
2021-08-14 05:21:00 -0400

    substring method now accepts text not just a word

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,16 @@
-def substrings(word, dictionary)
+def substrings(text, dictionary)
+  words = text.downcase.split(" ")
   substring_hash = Hash.new(0)
-  dictionary.each do |elem|
-    substring_hash[elem] += 1 if word.include?(elem)
+  words.each do |word|
+    dictionary.each do |elem|
+      substring_hash[elem] += 1 if word.include?(elem.downcase)
+    end
   end
   substring_hash
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
+#puts substrings("below", dictionary)
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
