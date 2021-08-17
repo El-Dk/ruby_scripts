0e740bad386047724a3b80cce04fbf4dbd5d1117
Derek <briannish@gmail.com>
2021-08-17 15:35:44 -0400

    Approach changed, updated method caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,34 +1,21 @@
-require "pry-byebug"
+# frozen_string_literal: false
+
 def caesar_cipher(text, shift_factor)
-  for i in 0...text.length
+  shift_factor %= 26
+  (0...text.length).each do |i|
+    next unless text[i].match?(/[a-zA-Z]/)
+
     char_ascii = text[i].ord
-    if char_ascii.between?(97, 122)
-      char_ascii += shift_factor
-      if char_ascii > 122
-        until char_ascii <= 122
-          char_ascii -= 26
-        end 
-      elsif char_ascii < 97
-        until char_ascii >= 97
-          char_ascii += 26
-        end 
-      end
-    elsif char_ascii.between?(65, 90)
-      char_ascii += shift_factor
-      
-      if char_ascii > 90
-        until char_ascii <= 90
-          char_ascii -= 26
-        end 
-      elsif char_ascii < 65
-        until char_ascii >= 65
-          char_ascii += 26
-        end 
-      end
-    end
+    char_ascii += shift_factor
+    char_ascii -= 26 unless char_ascii.between?(97, 122) ||
+                            char_ascii.between?(65, 90)
     text[i] = char_ascii.chr
   end
   text
 end
 
-puts caesar_cipher("Hola MundozZ!", -5)
+puts caesar_cipher('What a string!', 5)
+puts caesar_cipher('What a string!', 31)
+puts caesar_cipher('What a string!', 57)
+puts caesar_cipher('What a string!', -21)
+puts caesar_cipher('What a string!', 6)
